387ed31a890b2f66382f4eb5df0a03773d04a6ad
Vanya Shikhalev <nikittav5@gmail.com>
2024-09-13 21:37:28 +0300

    tried to fix connection to db

--- src/storage/connection.py
+++ src/storage/connection.py
@@ -16,19 +16,21 @@ class DBConection():
                 raise ConnectionError(f'{cls.__name__!r} not initiated!')
             instance = super().__new__(cls)
             instance._connection = mysql.Connection(**kwargs)
-            instance._cursor = instance._connection.cursor()
-            instance._c = instance._cursor
+            # instance._cursor = instance._connection.cursor()
+            # instance._c = instance._cursor
             cls.__instance = instance
         return cls.__instance
 
     def _execute(self, query: str) -> None:
         (loger.debug if query.strip().lower().startswith('select') else loger.info)(f'Executing query: {query}')
+        self._c = self._connection.cursor()
         self._c.execute(query)
         self._connection.commit()
     
     def query(self, query: str) -> tuple[None | tuple[Any]]:
         self._execute(query)
         result = self._c.fetchall()
+        self._c.close()
         loger.debug(f'Return: {result}')
         return result
     
